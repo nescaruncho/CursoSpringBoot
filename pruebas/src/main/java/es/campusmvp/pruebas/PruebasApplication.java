f84c807e7979b80f92b598ffbd3b9c25247fc105
nescaruncho <nescaruncho@gmail.com>
2025-01-27 14:48:28 +0100

    Módulo 3 vacío

--- pruebas/src/main/java/es/campusmvp/pruebas/PruebasApplication.java
+++ pruebas/src/main/java/es/campusmvp/pruebas/PruebasApplication.java
@@ -1,22 +1,13 @@
 package es.campusmvp.pruebas;
 
-import es.campusmvp.pruebas.motor.coches.Coche;
-import es.campusmvp.pruebas.motor.coches.CocheElectrico;
-import es.campusmvp.pruebas.motor.coches.CocheGasolina;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class PruebasApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(PruebasApplication.class, args);
-
-		Coche coche = context.getBean(Coche.class);
-		coche.arrancarTodo();
-		coche.apagarTodo();
-
+		SpringApplication.run(PruebasApplication.class, args);
 	}
 
 }
